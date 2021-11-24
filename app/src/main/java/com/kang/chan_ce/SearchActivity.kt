9232b271c96bd4f81a01d175665293ee32ac7273
KangInyeong <52899340+KangInyeong@users.noreply.github.com>
2021-11-24 11:26:57 +0900

    Commit during recyclerview for search function with using Firestore

--- app/src/main/java/com/kang/chan_ce/SearchActivity.kt
+++ app/src/main/java/com/kang/chan_ce/SearchActivity.kt
@@ -2,25 +2,73 @@ package com.kang.chan_ce
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.kang.chan_ce.databinding.ActivityMainBinding
-import com.kang.chan_ce.databinding.ActivitySearchBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.item.*
+import kotlinx.android.synthetic.main.item.view.*
 
 class SearchActivity:AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-        val binding = ActivitySearchBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    var firestore : FirebaseFirestore? = null
 
-        binding.btnStore1.setOnClickListener {
-            val intent = Intent(this, StoreDetailActivity::class.java)
-            startActivity(intent)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        firestore = FirebaseFirestore.getInstance()
+
+        recyclerview.adapter = RecyclerViewAdapter()
+        recyclerview.layoutManager = LinearLayoutManager(this)
+    }
+
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        var storeList : ArrayList<StoreData> = arrayListOf()
+
+        init {
+            firestore?.collection("StoreList")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+
+                storeList.clear()
+
+                for (snapshot in querySnapshot!!.documents) {
+                    var item = snapshot.toObject(StoreData::class.java)
+                    storeList.add(item!!)
+                }
+                notifyDataSetChanged()
+            }
         }
 
-        binding.btnBack.setOnClickListener{
-            finish()
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+            return ViewHolder(view)
+        }
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var viewHolder = (holder as ViewHolder).itemView
+
+//            Glide.with(viewHolder).load(storeList[position].storeImage).into(viewHolder.storeImage)
+//            Log.e("안녕 여기 로그좀","$storeImage ${storeList[position].storeImage}")
+
+            viewHolder.storeName.text = storeList[position].storeName
+            viewHolder.storeLocation.text = storeList[position].storeLocation
+            viewHolder.storeTime.text = storeList[position].storeTime
+        }
+
+        override fun getItemCount(): Int {
+            return storeList.size
         }
     }
+
 }
---
+++ app/src/main/java/com/kang/chan_ce/StoreData.kt
@@ -0,0 +1,8 @@
+package com.kang.chan_ce
+
+data class StoreData(
+    val storeImage : String? = null,
+    val storeName : String? = null,
+    val storeLocation : String? =null,
+    val storeTime : String? = null
+)
